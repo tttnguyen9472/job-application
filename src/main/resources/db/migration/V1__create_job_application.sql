a5dd527efa8cab138f58e8611946e3fc677e502d
Nguyen Thanh Tung <tttnguyen.sport@gmail.com>
2021-05-06 22:06:03 +0200

    JA-1 Flyway scripts are ready

---
+++ src/main/resources/db/migration/V1__create_job_application.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Jobs.job_application
+(
+    company_name    varchar(16)     not null,
+    job_title       varchar(30)     not null,
+    application_date    date        not null,
+    application_status  varchar(16),
+    salary          bigint,
+    offer           bit,
+    primary key (company_name, job_title)
+);
---
+++ src/main/resources/db/migration/V2__add_job_applications.sql
@@ -0,0 +1,10 @@
+INSERT INTO Jobs.job_application (company_name, job_title, application_date)
+VALUES ('Lufthansa', 'Java Developer', '2021-05-06');
+INSERT INTO Jobs.job_application (company_name, job_title, application_date)
+VALUES ('EPAM', 'Frontend Developer', '2021-05-06');
+INSERT INTO Jobs.job_application (company_name, job_title, application_date)
+VALUES ('Accenture', 'Full-Stack Developer', '2021-05-04');
+INSERT INTO Jobs.job_application (company_name, job_title, application_date)
+VALUES ('IBM', 'DevOps Engineer', '2021-05-05');
+INSERT INTO Jobs.job_application (company_name, job_title, application_date)
+VALUES ('Facebook', 'Frontend Developer', '2021-05-02');
